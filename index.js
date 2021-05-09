19e2317b219dd49c9b91c338079d5aa066ddea66
Akasam79 <emmachukwuebuka2@gmail.com>
2021-05-09 22:29:54 +0100

    working with mongo atlas and heroku

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+
+const MongoClient = require("mongodb").MongoClient;
+
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const connectionString = process.env.MONGODB_URI;
+
+mongoose.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log("failed");
+    } else {
+      console.log("database connected successfully");
+    }
+  }
+);
+
+const ClientSchema = new Schema({
+  name: String,
+  email: String,
+  country: String,
+});
+
+const Client = mongoose.model("Client", ClientSchema);
+
+app.get("/client", (req, res) => {
+  Client.find({}, (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal Server error" });
+    } else {
+      return res.status(200).json({ message: result });
+    }
+  });
+});
+
+app.post("/", (req, res) => {
+  Client.insertMany(
+    {
+      name: req.body.name,
+      email: req.body.email,
+      country: req.body.country,
+    },
+    (err, result) => {
+      if (err)
+        return res.status(500).json({ message: "Internal Server error" });
+      return res.status(200).json({ message: "New client added successfully" });
+    }
+  );
+});
+
+app.put("/client", (req, res) => {
+  var user_id = req.body.id;
+  var clientUpdates = {
+    name: req.body.name,
+    email: req.body.email,
+    country: req.body.country,
+  };
+  Client.findByIdAndUpdate(user_id, clientUpdates, (err, result) => {
+    if (err) return res.status(500).json({ message: "Internal Server error" });
+    return res.status(200).json({ message: "Data updated sucessfully" });
+  });
+});
+
+app.delete("/client", (req, res) => {
+  var user_id = req.body.id;
+  Client.findByIdAndDelete(user_id, (err, result) => {
+    if (err) return res.status(500).json({ message: "Internal server error" });
+    return res.status(200).json({ message: "Client deleted sucessfully" });
+  });
+});
