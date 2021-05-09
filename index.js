5cfb21ba7e07401b8b3558ffcc4ee7dd066d88d6
Akasam79 <emmachukwuebuka2@gmail.com>
2021-05-09 23:47:04 +0100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
 
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
